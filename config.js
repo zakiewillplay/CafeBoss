fe11c286cde7dfb4134e41cef02cd0fe93580461
zakiewillplay <zakiewillplay@gmail.com>
2026-05-19 17:25:21 +0530

    update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 export const SUPABASE_URL = 'https://udlmyspetkjzcdkfshgl.supabase.co';
-// Notice the lowercase "e" at the very start of the key below!
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkbG15c3BldGtqemNka2ZzaGdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxODIzMDksImV4cCI6MjA5NDc1ODMwOX0.oQL6hbwjQlU_2h4xcbD-v1yl7EubMz90VpIpTPBwEM4'; 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
