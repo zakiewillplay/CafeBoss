2bbc5b7e1b47e7326caded538d58d7bf122e61c4
zakiewillplay <zakiewillplay@gmail.com>
2026-05-19 23:09:15 +0530

    update sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'cafe-boss-v1';
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Required empty fetch handler for PWA installation
+});
